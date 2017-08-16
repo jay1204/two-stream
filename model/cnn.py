545c74e9da2b06a3d0646e039db9768d940d11e4
jay1204 <zchen22@ncsu.edu>
2017-08-16 14:32:37 -0400

    change to push accuracy value instead of a tuple to train_acc list

--- model/cnn.py
+++ model/cnn.py
@@ -74,13 +74,13 @@ class CNN(object):
                 mod.backward()
                 mod.update()
                 count += 1
-                train_acc.append(metric.get())
+                train_acc.append(metric.get()[1])
                 if count % 100 == 0:
                     # logger.info("The train accuracy of the %d-th iteration is %f"%(count, train_acc[-1][1]))
-                    print "The train accuracy of the %d-th iteration is %f"%(count, train_acc[-1][1])
+                    print "The train accuracy of the %d-th iteration is %f"%(count, train_acc[-1])
                     score = mod.score(valid_iter.next(), ['acc'])
-                    valid_acc.append(score)
-                    print "The valid accuracy of the %-th iteration is %f"%(count, valid_acc[-1][1])
+                    valid_acc.append(score[1])
+                    print "The valid accuracy of the %-th iteration is %f"%(count, valid_acc[-1])
                     # logger.info("The valid accuracy of the %-th iteration is %f"%(count, valid_acc[-1][1]))
                     if valid_acc[-1] > valid_accuracy:
                         valid_accuracy = valid_acc[-1]
